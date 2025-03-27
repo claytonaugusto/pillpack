7a7ccbed65a2bc9b573a6f740833631b3e6399e3
Clayton Augusto <clayton@avant.com.br>
2025-03-27 11:04:35 -0300

    Página de dashboard criada e com botão de cadastro escondido

--- public/dashboard.php
+++ public/dashboard.php
@@ -11,15 +11,35 @@
             <!-- mapa de páginas e botão de cadastro -->
             <div class="flex justify-between items-center min-h-20 p-1.5 w-full mb-5">
                 <h2>Home / <a href="#" class="text-(--bg-btn-hover)">Dashboard</a></h2>
-                <button class="bg-(--bg-blue-green) p-2.5 w-3xs block cursor-pointer rounded-lg hover:bg-(--bg-blue-green-hover) duration-300"><a href="" class="text-(--bg-baby-powder)"><i class="fa-solid fa-plus"></i>Cadastrar usuário</a></button>
+                <!-- <button class="bg-(--bg-blue-green) p-2.5 w-3xs block cursor-pointer rounded-lg hover:bg-(--bg-blue-green-hover) duration-300"><a href="" class="text-(--bg-baby-powder)"><i class="fa-solid fa-plus"></i>Cadastrar usuário</a></button> -->
             </div>
 
             <!-- dashboard cards -->
             <div class="flex flex-wrap justify-start w-full p-1.5 mt-4 gap-2.5">
 
-                <div class="flex flex-col items-center justify-center w-74 h-36 bg-(--bg-btn) rounded-lg cursor-pointer hover:bg-(--bg-btn-hover) duration-300">
-                    <h2 class="text-xl font-bold text-(--bg-baby-powder) mb-2">Novos clientes</h2>
-                    <h2 class="text-4xl font-bold text-(--bg-baby-powder) mb-2">30</h2>
+                <div class="flex flex-col items-center justify-center w-74 h-36 bg-(--bg-yellow-avanti) rounded-lg cursor-pointer text-(--text-prussian-blue) block hover:text-(--bg-seasalt) hover:bg-(--bg-btn-hover) duration-300">
+                    <h2 class="text-xl font-bold mb-2">Novos clientes</h2>
+                    <h2 class="text-4xl font-bold mb-2">30</h2>
+                </div>
+
+                <div class="flex flex-col items-center justify-center w-74 h-36 bg-(--bg-yellow-avanti) rounded-lg cursor-pointer text-(--text-prussian-blue) block hover:text-(--bg-seasalt) hover:bg-(--bg-btn-hover) duration-300">
+                    <h2 class="text-xl font-bold mb-2">Clientes totais</h2>
+                    <h2 class="text-4xl font-bold mb-2">60</h2>
+                </div>
+
+                <div class="flex flex-col items-center justify-center w-74 h-36 bg-(--bg-yellow-avanti) rounded-lg cursor-pointer text-(--text-prussian-blue) block hover:text-(--bg-seasalt) hover:bg-(--bg-btn-hover) duration-300">
+                    <h2 class="text-xl font-bold mb-2">Clientes uso contínuo</h2>
+                    <h2 class="text-4xl font-bold mb-2">26</h2>
+                </div>
+
+                <div class="flex flex-col items-center justify-center w-74 h-36 bg-(--bg-yellow-avanti) rounded-lg cursor-pointer text-(--text-prussian-blue) block hover:text-(--bg-seasalt) hover:bg-(--bg-btn-hover) duration-300">
+                    <h2 class="text-xl font-bold mb-2 text-center">Receitas próximas do vencimento</h2>
+                    <h2 class="text-4xl font-bold mb-2">13</h2>
+                </div>
+
+                <div class="flex flex-col items-center justify-center w-74 h-36 bg-(--bg-yellow-avanti) rounded-lg cursor-pointer text-(--text-prussian-blue) block hover:text-(--bg-seasalt) hover:bg-(--bg-btn-hover) duration-300">
+                    <h2 class="text-xl font-bold mb-2">ILPI's</h2>
+                    <h2 class="text-4xl font-bold mb-2">12</h2>
                 </div>
 
 
